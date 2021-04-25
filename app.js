180d37d6aee7359d1a50a31a496af658bed63728
Sean Nguyen <sean.nguyenf@yahoo.com>
2021-04-24 23:26:57 -0400

    understanding types and type inference

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Your code goes here...");
+var add = function (n1, n2, showResult, phrase) {
+    // console.log(typeof number1);
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+};
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = "Result is: ";
+add(number1, number2, printResult, resultPhrase);
