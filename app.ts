518c66d9561469f84ae154ea45706979ad6f2cf6
Sean Nguyen <sean.nguyenf@yahoo.com>
2021-04-27 21:18:54 -0400

    function types and callbacks

--- app.ts
+++ app.ts
@@ -22,6 +22,17 @@ let combineValues: (a: number, b: number) => number;
 
 combineValues = add;
 // combineValues = 5;
-combineValues = printResult;
+// combineValues = printResult;
 
 console.log(combineValues(8, 8));
+
+
+// Function Types and CallBacks
+const addAndHandle = (n1: number, n2: number, cb: (num: number) => void) => {
+  const result = n1 + n2;
+  cb(result);
+};
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
