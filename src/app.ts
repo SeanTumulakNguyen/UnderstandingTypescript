d1feea9d0d97121860c3616d2fc33081f7d2bca6
Sean Nguyen <sean.nguyenf@yahoo.com>
2021-05-05 22:05:37 -0400

    section 5 constructor

--- src/app.ts
+++ src/app.ts
@@ -1,84 +1,17 @@
-// Code goes here
+// object orient programming (oop)
+// work with real-life entities in code
 
-const userName = "Max";
-// constants can't be changed
-// let age = 30;
+// classes are bluprints for objects
+// define how objects look like, properties and methods they have
+// make creation of multiple, similar objects much easier
 
-// age = 29;
+class Department {
+  name: string;
 
-// // console.log(result)
-// if (age > 20) {
-//   let isOld = true;
-// }
-
-// fails in typescipt and not javascript
-// if using var, variable is able to seen globally
-// console.log(isOld);
-
-// let and const allows block scope
-
-// arrow function
-const addForLet = (a: number, b: number) => a + b;
-
-console.log(addForLet(2, 5));
-
-const printOutput: (a: number | string) => void = (output) =>
-  console.log(output);
-
-printOutput(addForLet(5, 2));
-
-const buttonPress = document.querySelector("button");
-
-if (buttonPress) {
-  // if no parameters, use have to use pair of empty parentheses
-  buttonPress.addEventListener("click", (event) => {
-    console.log(event);
-  });
+  constructor(n: string) {
+    this.name = n;
+  }
 }
 
-// default function parameters
-// so that only one parameter is needed
-const ticket = (a: number, b: number = 1) => a + b;
-// order of parameters matter
-printOutput(ticket(6));
-
-const hobbies = ["Sports", "Cooking"];
-const activeHobbies = ["Hiking"];
-
-// using spread operator
-activeHobbies.push(...hobbies);
-
-const aPerson = {
-  firstName: "Max",
-  age: 30,
-};
-
-// pulling all the key pairs from aPerson
-const copiedPerson = { ...aPerson };
-
-// rest parameters
-const addRest = (...numbers: number[]) => {
-  // using reduce method
-  // two parameters, the result and then the value
-  // reduce does function on every item in array
-  return numbers.reduce((curResult, curValue) => {
-    return curResult + curValue;
-  }, 0);
-};
-
-// can use tuples such as [number, number, number] to only take three parameters
-
-const addRestNumbers = addRest(5, 10, 2, 3.7);
-console.log(addRestNumbers);
-
-// array and object destructuring
-
-// array destructuring
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
-
-console.log(hobbies, hobby1, hobby2);
-
-// object destructuring
-const { firstName: userFirstName, age } = aPerson;
-
-console.log(userFirstName);
+const accounting = new Department("Accounting");
+console.log(accounting);
