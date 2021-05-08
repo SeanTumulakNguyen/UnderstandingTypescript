2b1afde3f8381330612585fbc9a41f0ba70554ba
Sean Nguyen <sean.nguyenf@yahoo.com>
2021-05-08 13:25:22 -0400

    type guards

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,53 @@
 "use strict";
 // intersection types
 const e1 = {
-    name: 'Max',
-    priviledges: ['create-server'],
-    startDate: new Date()
+    name: "Max",
+    priviledges: ["create-server"],
+    startDate: new Date(),
 };
+// type guards
+const AddGuard = (a, b) => {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+};
+const printEmployeeInformation = (emp) => {
+    console.log("Name: " + emp.name);
+    // using the 'in' keyword checks if string exists in emp
+    // javascript
+    if ("priviledges" in emp) {
+        console.log("Priviledges: " + emp.priviledges);
+    }
+    if ("startDate" in emp) {
+        console.log("Start Date: " + emp.startDate);
+    }
+};
+printEmployeeInformation({ name: "Manu", startDate: new Date() });
+// instance of type guard
+class Car {
+    drive() {
+        console.log("Driving...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving a truck...");
+    }
+    loadCargo(amount) {
+        console.log("Loading cargo " + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+const useVehicle = (vehicle) => {
+    vehicle.drive();
+    // instanceof is part of javascript
+    // instanceof could not be used in interfaces because javascript does not compile interfaces
+    // this would only be able to be used with classes
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+};
+useVehicle(v1);
+useVehicle(v2);
--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,89 @@
 
 // could use interface types and extending the interfaces down below as well
 type Admin = {
-    name: string;
-    priviledges: string[];
+  name: string;
+  priviledges: string[];
 };
 
 type Employee = {
-    name: string;
-    startDate: Date;
-}
+  name: string;
+  startDate: Date;
+};
 
 // interface ElevatedEmployee extends Admin, Employee { }
 
 type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
-    name: 'Max',
-    priviledges: ['create-server'],
-    startDate: new Date()
-}
+  name: "Max",
+  priviledges: ["create-server"],
+  startDate: new Date(),
+};
 
 type CombinableIntersection = string | number;
-type Numeric = number | boolean
+type Numeric = number | boolean;
 
-type Universal = CombinableIntersection & Numeric
+type Universal = CombinableIntersection & Numeric;
+
+// type guards
+const AddGuard = (a: CombinableIntersection, b: CombinableIntersection) => {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+};
+
+type UnknownEmployeee = Employee | Admin;
+
+const printEmployeeInformation = (emp: UnknownEmployeee) => {
+  console.log("Name: " + emp.name);
+  // using the 'in' keyword checks if string exists in emp
+  // javascript
+  if ("priviledges" in emp) {
+    console.log("Priviledges: " + emp.priviledges);
+  }
+
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+};
+
+printEmployeeInformation({ name: "Manu", startDate: new Date() });
+
+// instance of type guard
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo " + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+
+const v2 = new Truck();
+
+const useVehicle = (vehicle: Vehicle) => {
+    vehicle.drive();
+    // instanceof is part of javascript
+    // instanceof could not be used in interfaces because javascript does not compile interfaces
+    // this would only be able to be used with classes
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+};
+
+useVehicle(v1);
+
+useVehicle(v2);
