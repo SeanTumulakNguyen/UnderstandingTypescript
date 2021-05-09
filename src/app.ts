fed7f90fe24c14edf91c212baafe5e66be3807ba
Sean Nguyen <sean.nguyenf@yahoo.com>
2021-05-09 09:27:27 -0400

    creating generic function

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,24 @@
 // generic type
 
-const names: Array<string> = []; // string[]
+// const names: Array<string> = []; // string[]
 // names[0].split(' ');
 
 // promise type
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("This is done!");
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  data.split(" ");
-});
+// promise.then((data) => {
+//   data.split(" ");
+// });
+
+// creating generic function
+const merge = <T, U>(objA: T, objB: U) => {
+  return Object.assign(objA, objB);
+};
+
+// console.log(merge({ name: "Max" }, { age: 30 }));
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+console.log(mergedObj.age)
