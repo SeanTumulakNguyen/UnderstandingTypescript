2395e7feeb3a2b8bffda8183c2f1b86445ce7bd6
Sean Nguyen <sean.nguyenf@yahoo.com>
2021-06-04 00:30:47 -0400

    section 9 start

--- dist/app.js
+++ dist/app.js
@@ -1,100 +1,14 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-// decorator factories
-const Logger = (logString) => {
-    console.log("LOGGER FACTORY");
-    return function (constructor) {
-        console.log(logString);
-        console.log(constructor);
-    };
-};
-// template decorator
-const withTemplate = (template, hookId) => {
-    console.log("LOGGER TEMPLATE");
-    return function (originalConstructor) {
-        return class extends originalConstructor {
-            constructor(..._) {
-                super();
-                console.log("Rendering template");
-                const hookEl = document.getElementById(hookId);
-                if (hookEl) {
-                    hookEl.innerHTML = template;
-                    hookEl.querySelector("h1").textContent = this.name;
-                }
-            }
-        };
-    };
-};
-// decorators run bottom up
-let PersonDecorator = class PersonDecorator {
+class ProjectInput {
     constructor() {
-        this.name = "Max";
-        console.log("Creating person object...");
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
     }
-};
-PersonDecorator = __decorate([
-    Logger("LOGGING"),
-    withTemplate("<h1>My Person Object</h1>", "app")
-], PersonDecorator);
-// const pers = new PersonDecorator();
-// console.log(pers);
-// ---
-const Log = (target, propertyName) => {
-    console.log("Property decorator!");
-    console.log(target, propertyName);
-};
-const Log2 = (target, name, descriptor) => {
-    console.log("Accessor decorator!");
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-};
-const Log3 = (target, name, descriptor) => {
-    console.log("Method decorator!");
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-};
-const Log4 = (target, name, position) => {
-    console.log("Parameter decorator!");
-    console.log(target);
-    console.log(name);
-    console.log(position);
-};
-class Product {
-    constructor(t, p) {
-        this.title = t;
-        this._price = p;
-    }
-    set price(val) {
-        if (val > 0) {
-            this._price = val;
-        }
-        else {
-            throw new Error("Invalid price - should be positive!");
-        }
-    }
-    getPriceWithTax(tax) {
-        return this._price * (1 + tax);
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
     }
 }
-__decorate([
-    Log
-], Product.prototype, "title", void 0);
-__decorate([
-    Log2
-], Product.prototype, "price", null);
-__decorate([
-    Log3,
-    __param(0, Log4)
-], Product.prototype, "getPriceWithTax", null);
-const p1 = new Product("Book", 19);
-const p2 = new Product("Book 2", 29);
+const prjInput = new ProjectInput();
