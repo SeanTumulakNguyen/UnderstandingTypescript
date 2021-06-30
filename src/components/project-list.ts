1be9b3257c44a659ef738424680c5d465396dfba
Sean Nguyen <sean.nguyenf@yahoo.com>
2021-06-29 23:51:56 -0400

    adding entry and output configuration

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,9 +1,9 @@
-import Component from "./base-component.js";
-import { autobind } from "../decorators/autobind.js";
-import { Project, ProjectStatus } from "../models/project.js";
-import { DragTarget } from "../models/drag-drop.js";
-import { ProjectItem } from "./project-item.js";
-import { projectState } from "../state/project-state.js";
+import Component from "./base-component";
+import { autobind } from "../decorators/autobind";
+import { Project, ProjectStatus } from "../models/project";
+import { DragTarget } from "../models/drag-drop";
+import { ProjectItem } from "./project-item";
+import { projectState } from "../state/project-state";
 
 // ProjectList Class
 export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
