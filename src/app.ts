305ae1be066cd166532632a5c6a24eb2bf46fa64
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2021-07-03 00:21:59 -0400

    class-transformer library

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import "reflect-metadata"
+import {plainToClass} from 'class-transformer'
+
+import { ProductSectionTwelve } from "product.model";
+
+const p1 = new ProductSectionTwelve('A Book', 12.99);
+
+console.log(p1.getInformation())
