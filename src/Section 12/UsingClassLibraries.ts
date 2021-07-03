78cdb87fd767ce862d2f6ce8a8110f26d4f8a6b0
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2021-07-03 00:37:46 -0400

    start of section 13

---
+++ src/Section 12/UsingClassLibraries.ts
@@ -0,0 +1,9 @@
+import "reflect-metadata"
+import {plainToClass} from 'class-transformer'
+import {} from 'class-validator'
+
+import { ProductSectionTwelve } from "./product.model";
+
+const p1 = new ProductSectionTwelve('A Book', 12.99);
+
+console.log(p1.getInformation())
--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import "reflect-metadata"
-import {plainToClass} from 'class-transformer'
-import {} from 'class-validator'
-
-import { ProductSectionTwelve } from "product.model";
-
-const p1 = new ProductSectionTwelve('A Book', 12.99);
-
-console.log(p1.getInformation())
