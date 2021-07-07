597b521028c81af1d1f8d687b7a34eb4c85a0f91
Sean Nguyen <sean.nguyenf@yahoo.com>
2021-07-06 22:39:57 -0400

    start typescript and node js

---
+++ src/section15/dist/controllers/todos.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createTodo = void 0;
+const todo_1 = require("../models/todo");
+const TODOS = [];
+const createTodo = (req, res, next) => {
+    const text = req.body.text;
+    const newTodo = new todo_1.Todo(Math.random().toString(), text);
+};
+exports.createTodo = createTodo;
